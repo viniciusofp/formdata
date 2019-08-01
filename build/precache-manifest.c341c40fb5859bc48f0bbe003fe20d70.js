106d51496c853478604c0e227f6c0b11f2759dbb
viniciusofp <viniciusofp@gmail.com>
2019-08-01 17:23:52 -0300

    style

---
+++ build/precache-manifest.c341c40fb5859bc48f0bbe003fe20d70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae82d74e3b91d69e90941117ce68d8e",
+    "url": "/formdata/index.html"
+  },
+  {
+    "revision": "2e3277168966539f4f7a",
+    "url": "/formdata/static/css/main.48799dd6.chunk.css"
+  },
+  {
+    "revision": "65ba9c155366152aae40",
+    "url": "/formdata/static/js/2.50930c24.chunk.js"
+  },
+  {
+    "revision": "2e3277168966539f4f7a",
+    "url": "/formdata/static/js/main.cc2ce664.chunk.js"
+  },
+  {
+    "revision": "85da77c22d2faa2f7df3",
+    "url": "/formdata/static/js/runtime~main.73bc4fc9.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/formdata/precache-manifest.6be125d15f679650f668bc5eb09fd4e9.js"
+  "/formdata/precache-manifest.c341c40fb5859bc48f0bbe003fe20d70.js"
 );
 
 self.addEventListener('message', (event) => {
