4df1f0b7e063db10e2efba5c6b3af5c12d8be8d7
viniciusofp <viniciusofp@gmail.com>
2019-09-30 21:37:45 -0300

    form fix

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -22,7 +22,6 @@ class AuthForm extends Component {
             id="userInput"
             type="text"
             className="form-control"
-            defaultValue="creativexapiuser"
             placeholder="Usuário"
             name="username"
           />
@@ -33,7 +32,6 @@ class AuthForm extends Component {
             id="passInput"
             type="password"
             className="form-control"
-            defaultValue="lal(0op_86Ab^aI"
             placeholder="Senha"
             name="password"
           />
